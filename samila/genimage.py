db85e8fe59b714d92ec316253f69a3d37cf6c0e3
sadrasabouri <sabouri.sadra@gmail.com>
2021-09-27 10:48:06 +0330

    edit : pep8ed.

--- samila/genimage.py
+++ samila/genimage.py
@@ -6,13 +6,19 @@ import matplotlib.pyplot as plt
 from .functions import float_range
 from .params import *
 
+
 class GenerativeImage:
 
     def __init__(self, function1, function2):
         self.function1 = function1
         self.function2 = function2
 
-    def generate(self, seed=None, start=DEFAULT_START, step=DEFAULT_STEP, stop=DEFAULT_STOP):
+    def generate(
+            self,
+            seed=None,
+            start=DEFAULT_START,
+            step=DEFAULT_STEP,
+            stop=DEFAULT_STOP):
         self.data1 = []
         self.data2 = []
         self.seed = seed
@@ -24,11 +30,14 @@ class GenerativeImage:
             self.data1.append(self.function1(item[0], item[1]))
             self.data2.append(self.function2(item[0], item[1]))
 
-    def plot(self, color=DEFAULT_COLOR, spot_size=DEFAULT_SPOT_SIZE, size=DEFAULT_IMAGE_SIZE, projection=DEFAULT_PROJECTION):
+    def plot(
+            self,
+            color=DEFAULT_COLOR,
+            spot_size=DEFAULT_SPOT_SIZE,
+            size=DEFAULT_IMAGE_SIZE,
+            projection=DEFAULT_PROJECTION):
         fig = plt.figure()
         fig.set_size_inches(size[0], size[1])
         ax = fig.add_subplot(111, projection=projection)
         ax.scatter(self.data2, self.data1, alpha=0.1, c=color, s=spot_size)
         ax.axis('off')
-
-
