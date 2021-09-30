f7dcab56c31bc555cdca72c2dc3e9a9ab32d4f32
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-09-30 14:59:34 +0330

    fix : __version__ added to init file

--- samila/__init__.py
+++ samila/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 """Samila modules."""
 from .genimage import GenerativeImage
-from .params import Projection, VALID_COLORS
+from .params import Projection, VALID_COLORS, SAMILA_VERSION
+__version__ = SAMILA_VERSION
