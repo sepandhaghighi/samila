42e58b38604da055c8d6ca11edda0a55494d5644
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-02 22:04:40 +0330

    add : main test added. (#42)

---
+++ samila/test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Basic test file."""
+"""
+>>> import random
+>>> import math
+>>> import matplotlib.pyplot as plt
+>>> from samila import GenerativeImage
+>>> def f1(x,y):
+...    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
+...    return result
+>>> def f2(x,y):
+...    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
+...    return result
+>>> g = GenerativeImage(f1,f2)
+>>> g.generate()
+>>> g.plot()
+
+"""
