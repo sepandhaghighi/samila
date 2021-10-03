6c3ad4a78a546c923af37ce1af65f5704dd1716c
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-04 00:23:25 +0330

    fix : minor issue fixed.

--- Test/function_test.py
+++ Test/function_test.py
@@ -5,14 +5,16 @@
 >>> from samila import GenerativeImage, Projection
 >>> import pickle
 >>> def f1(x,y):
-    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
-    return result
+...    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
+...    return result
 >>> def f2(x,y):
-    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
-    return result
+...    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
+...    return result
 >>> g = GenerativeImage(f1,f2)
 >>> g.function1 == f1
+True
 >>> g.function2 == f2
+True
 >>> g.fig
 >>> g.generate()
 >>> isinstance(g.data1, list)
@@ -22,21 +24,25 @@ True
 >>> g.generate(seed=0)
 >>> g.seed
 0
->>> with open("test1_0_d1.pkl", "rb") as fp:
+>>> with open("Test/test1_0_d1.pkl", "rb") as fp:
 ...   temp_data = pickle.load(fp)
 >>> temp_data == g.data1
->>> with open("test1_0_d2.pkl", "rb") as fp:
+True
+>>> with open("Test/test1_0_d2.pkl", "rb") as fp:
 ...   temp_data = pickle.load(fp)
 >>> temp_data == g.data2
+True
 >>> g.generate(seed=10, start=-2*math.pi, step=0.1, stop=math.pi/2)
 >>> g.seed
 10
->>> with open("test1_1_d1.pkl", "rb") as fp:
+>>> with open("Test/test1_1_d1.pkl", "rb") as fp:
 ...   temp_data = pickle.load(fp)
 >>> temp_data == g.data1
->>> with open("test1_1_d2.pkl", "rb") as fp:
+True
+>>> with open("Test/test1_1_d2.pkl", "rb") as fp:
 ...   temp_data = pickle.load(fp)
 >>> temp_data == g.data2
+True
 >>> g.plot()
 >>> g.plot(color='red')
 >>> g.plot(color='red', bgcolor='black')
