d8af6e02d1262b5c000fa6d2de5cab19ceeb6881
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-10-12 20:07:23 +0330

    fix : tests updated #53

--- test/function_test.py
+++ test/function_test.py
@@ -1,92 +1,31 @@
 # -*- coding: utf-8 -*-
 """
->>> import random
->>> import math
->>> import os
->>> import pickle
->>> import socket
->>> def guard(*args, **kwargs):
-...     raise Exception("No internet connection!")
->>> from samila import GenerativeImage, Projection
->>> from samila.functions import is_same_data
->>> import pickle
->>> def f1(x,y):
-...    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
-...    return result
->>> def f2(x,y):
-...    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
-...    return result
->>> g = GenerativeImage(f1,f2)
->>> g.function1 == f1
+>>> from samila.functions import *
+>>> s = list(float_range(1,1.5,0.1))
+>>> s
+[1.0, 1.1, 1.2000000000000002, 1.3000000000000003, 1.4000000000000004]
+>>> is_same_data(s,[1,1.1,1.2,1.3,1.4])
 True
->>> g.function2 == f2
-True
->>> g.fig
->>> g.generate()
->>> isinstance(g.data1, list)
-True
->>> isinstance(g.data2, list)
-True
->>> g.generate(seed=10, start=-2*math.pi, step=0.1, stop=math.pi/2)
->>> g.seed
-10
->>> with open("test/test1_1_d1.pkl", "rb") as fp:
-...    temp_data = pickle.load(fp)
->>> is_same_data(g.data1, temp_data)
-True
->>> with open("test/test1_1_d2.pkl", "rb") as fp:
-...    temp_data = pickle.load(fp)
->>> is_same_data(g.data2, temp_data)
-True
->>> g.plot()
->>> result = g.save_image("test.png")
->>> result["status"]
-True
->>> result["message"]
-'Everything seems good.'
->>> g.plot(color='red')
->>> g.plot(color='red', bgcolor='black')
->>> result = g.save_image("test2.png")
->>> result["status"]
-True
->>> result["message"]
-'Everything seems good.'
->>> from samila import GenerativeImage, Projection
->>> g.plot(projection=Projection.POLAR, color='red', bgcolor='black')
->>> g.plot(projection=Projection.POLAR, color=(.1, .2, .8))
->>> g.plot(bgcolor=(.1, .2, .8), spot_size=0.1)
->>> g.plot(size=(20, 20))
->>> result = g.nft_storage(api_key="")
->>> result['status']
+>>> is_same_data([1,1.1,1.2,1.3,1.4],[1,1.11,1.3,1.4,1.5])
 False
->>> result['message']
-'API Key is missing, make sure the `Authorization` header has a value in the following format `Bearer {api key}`.'
->>> g = GenerativeImage(f1,f2)
->>> result = g.nft_storage(api_key="")
->>> result["status"]
-False
->>> result["message"]
-'No figure was found. First run `generate` and `plot` methods.'
->>> result = g.save_image(file_adr="")
->>> result["status"]
-False
->>> result["message"]
-'No figure was found. First run `generate` and `plot` methods.'
->>> g.fig = 2
->>> result = g.nft_storage(api_key="")
->>> result['status']
-False
->>> result = g.save_image(file_adr="")
->>> result["status"]
-False
->>> socket.socket = guard
->>> g.generate()
->>> g.plot()
->>> result = g.nft_storage("")
->>> result["status"]
-False
->>> result["message"]
-'No internet connection!'
->>> os.remove("test.png")
->>> os.remove("test2.png")
+>>> filter_color("yellow")
+'yellow'
+>>> filter_color((0.2,0.3,0.4))
+(0.2, 0.3, 0.4)
+>>> filter_color(2)
+>>> filter_color(4)
+>>> distance_calc("test","test1")
+1
+>>> distance_calc("te1st","test")
+1
+>>> distance_calc("test12","test234")
+3
+>>> samila_help()
+<BLANKLINE>
+Samila is a generative art generator written in Python, Samila let's you
+create arts based on many thousand points. The position of every single
+point is calculated by a formula, which has random parameters.
+Because of the random numbers, every image looks different.
+<BLANKLINE>
+Repo : https://github.com/sepandhaghighi/samila
 """
