9e4f85e1fdde4534667dbac21d9ac269aab9740d
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-22 13:49:40 +0330

    add : error_test.py.

---
+++ test/error_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+>>> from samila import *
+>>> GI = GenerativeImage(data="data.json")
+Traceback (most recent call last):
+        ...
+samila.errors.samilaDataError: Provided data file is not supported. It should be either file or io.IOBase.
+"""
