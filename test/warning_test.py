b701f6ed91b8359fbd38565e93eeb5577f0b24f4
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-22 12:59:03 +0330

    add : warning_test.py.

---
+++ test/warning_test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+>>> from samila import *
+>>> from pytest import warns
+>>> with warns(RuntimeWarning, match='Neither function nor data is provided'):
+...     g = GenerativeImage()
+"""
