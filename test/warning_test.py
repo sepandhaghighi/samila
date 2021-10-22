d0e106221feca94585bcb571ac700fb83c244d56
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-22 14:10:00 +0330

    update : warning_test updated.

--- test/warning_test.py
+++ test/warning_test.py
@@ -7,8 +7,13 @@
 ...     g = GenerativeImage()
 >>> g = GenerativeImage(lambda x,y: 0, lambda x,y: 0)
 >>> g.generate(step=0.1)
+>>> g.plot()
 >>> g.save_data()
 >>> with warns(RuntimeWarning, match='Just data is provided you can't use generate function.'):
-...     g = GenerativeImage(data=open('data.json', 'r'))
+...     g_ = GenerativeImage(data=open('data.json', 'r'))
+>>> g_.plot()
+>>> g_.data1 == g.data1
+>>> g_.data2 == g.data2
+>>> g_.fig == g.fig
 >>> os.remove('data.json')
 """
