a5e9c9e14adf017e0855d0097220334148472a6d
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-22 13:54:44 +0330

    edit : parsing edited.

--- test/error_test.py
+++ test/error_test.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 >>> from samila import *
->>> GI = GenerativeImage(data="data.json")
+>>> import os
+>>> g = GenerativeImage(data="data.json")
 Traceback (most recent call last):
         ...
 samila.errors.samilaDataError: Provided data file is not supported. It should be either file or io.IOBase.
+>>> with open('data.json', 'r')
 """
