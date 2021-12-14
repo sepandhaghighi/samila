140ef8ace1ce930d2f69dd7b8a763a4056123e4e
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-12-14 18:58:27 +0330

    fix : nft_upload_test updated #32

--- test/nft_upload_test.py
+++ test/nft_upload_test.py
@@ -6,13 +6,7 @@
 >>> import time
 >>> from samila import GenerativeImage, Projection
 >>> from samila.params import VALID_COLORS
->>> def f1(x,y):
-...    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
-...    return result
->>> def f2(x,y):
-...    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
-...    return result
->>> g = GenerativeImage(f1,f2)
+>>> g = GenerativeImage()
 >>> g.generate()
 >>> g.plot()
 >>> NFT_STORAGE_API_KEY = os.environ["NFT_STORAGE_API_KEY"]
