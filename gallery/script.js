146e022bafcdd5de35ccc0d012d984817a6753e9
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-08-05 19:06:34 +0430

    gallery refresh button added

--- gallery/script.js
+++ gallery/script.js
@@ -1,3 +1,6 @@
+const image_error_url = '../images/image_error.png';
+var mydata = JSON.parse(data);
+
 function ready(callback) {
     if (document.readyState != 'loading') callback();
     else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
@@ -6,26 +9,38 @@ function ready(callback) {
     });
 }
 
-function ipfs_link(input_id) {
-    return "https://ipfs.io/ipfs/" + input_id
+function image_error(obj, id_number){
+    obj.src = image_error_url;
+    let desc = document.getElementById(`desc${id_number}`)
+    desc.innerHTML = `<i class="fa fa-refresh fa-lg refresh" aria-hidden="true" onclick="refresh(${id_number});"></i>`;
 }
 
-var mydata = JSON.parse(data);
+function ipfs_link(ipfs_id) {
+    return "https://ipfs.io/ipfs/" + ipfs_id
+}
+
+function refresh(id_number) {
+    let img = document.getElementById(`img${id_number}`)
+    img.src = ipfs_link(mydata[mydata.length - id_number]["link"])
+    let desc = document.getElementById(`desc${id_number}`)
+    desc.innerHTML = id_number;
+}
+
 
 var template = `
 <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="{{link}}">
-      <img src="{{link}}" alt="{{number1}}" width="300" height="300" loading="lazy" onerror="this.src='../images/image_error.png'">
+      <img id="img{{number1}}" src="{{link}}" alt="{{number1}}" width="300" height="300" loading="lazy" onerror="image_error(this, {{number1}});">
     </a>
-    <div class="desc">{{number1}}</div>
+    <div class="desc" id="desc{{number1}}">{{number1}}</div>
   </div>
 </div>
 `
 
 function fill_template(img_number, img_link) {
     var temp = template;
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 6; i++) {
         temp = temp.replace("{{number1}}", img_number).replace("{{link}}", img_link)
     }
     return temp
