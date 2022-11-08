1184c66982ff079288924fdd03b207532d536b71
sepandhaghighi <sepand.haghighi@yahoo.com>
2022-11-08 15:21:38 +0330

    fix : Python 3.11 added to setup.py

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     license='MIT',
     include_package_data=True
