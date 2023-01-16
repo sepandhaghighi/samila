42ee68bffcc381e4a8255f3002c4cb1a96875615
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-01-16 19:25:40 +0330

    fix : OPERATORS_LIST updated

--- samila/params.py
+++ samila/params.py
@@ -149,6 +149,6 @@ ARGUMENTS_LIST = [
     "x*(y**3)",
     "y*(x**3)"]
 
-OPERATORS_LIST = ["+", "-", "*", "/"]
+OPERATORS_LIST = ["+", "-", "*", "/", "%"]
 
 RANDOM_EQUATION_GEN_COMPLEXITY = len(ELEMENTS_LIST) + 1
