ce28a009aa2926e798ab371d5cd879e6520190ab
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-08-02 12:59:52 +0430

    fix : nft_upload_test.py updated

--- test/nft_upload_test.py
+++ test/nft_upload_test.py
@@ -1,64 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 >>> import os
->>> import time
 >>> from pytest import warns
 >>> from samila import GenerativeImage, Projection, Gateway
->>> from samila.params import VALID_COLORS
 >>> g = GenerativeImage()
 >>> g.generate()
 >>> g.plot()
 >>> NFT_STORAGE_API_KEY = os.environ["NFT_STORAGE_API_KEY"]
 >>> g.generate()
 >>> g.plot(projection=Projection.RANDOM, color="random", bgcolor="random")
->>> counter = 0
->>> try_limit = 10
->>> status = False
->>> result = {}
->>> while(status == False and counter<try_limit):
-...     with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
-...         result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, depth=3)
-...     counter = counter + 1
-...     status = result["status"]
-...     time.sleep(10)
->>> status
-True
->>> status = False
->>> counter = 0
->>> while(status == False and counter<try_limit):
-...     with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
-...         result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_config=True)
-...     counter = counter + 1
-...     status = all(result["status"].values())
-...     time.sleep(10)
+>>> with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
+...     result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, depth=3)
+>>> result["status"]
+False
+>>> with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
+...     result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_config=True)
 >>> result['status']["image"]
-True
+False
 >>> result['status']["config"]
-True
->>> status = False
->>> counter = 0
->>> while(status == False and counter<try_limit):
-...     with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
-...         result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True)
-...     counter = counter + 1
-...     status = all(result["status"].values())
-...     time.sleep(10)
+False
+>>>  with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
+...      result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True)
 >>> result['status']["image"]
-True
+False
 >>> result['status']["data"]
-True
->>> status = False
->>> counter = 0
->>> while(status == False and counter<try_limit):
-...     with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
-...         result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True, upload_config=True, gateway=Gateway.CID)
-...     counter = counter + 1
-...     status = all(result["status"].values())
-...     time.sleep(10)
+False
+>>>  with warns(DeprecationWarning, match='`test_deprecated` is deprecated and may be removed in future releases.'):
+...      result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True, upload_config=True, gateway=Gateway.CID)
 >>> result['status']["image"]
-True
+False
 >>> result['status']["data"]
-True
+False
 >>> result['status']["config"]
-True
+False
 """
