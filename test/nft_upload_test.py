17ac06a24990b6d3ab274e9da47d44bb066a46b3
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-08-03 06:56:40 +0430

    fix : tests indent bugs fixed

--- test/nft_upload_test.py
+++ test/nft_upload_test.py
@@ -19,14 +19,14 @@ False
 False
 >>> result['status']["config"]
 False
->>>  with warns(DeprecationWarning, match='`nft_storage` is deprecated and may be removed in future releases.'):
-...      result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True)
+>>> with warns(DeprecationWarning, match='`nft_storage` is deprecated and may be removed in future releases.'):
+...     result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True)
 >>> result['status']["image"]
 False
 >>> result['status']["data"]
 False
->>>  with warns(DeprecationWarning, match='`nft_storage` is deprecated and may be removed in future releases.'):
-...      result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True, upload_config=True, gateway=Gateway.CID)
+>>> with warns(DeprecationWarning, match='`nft_storage` is deprecated and may be removed in future releases.'):
+...     result = g.nft_storage(api_key=NFT_STORAGE_API_KEY, upload_data=True, upload_config=True, gateway=Gateway.CID)
 >>> result['status']["image"]
 False
 >>> result['status']["data"]
