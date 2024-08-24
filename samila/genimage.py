7158ca0bcbe1b8a376d21a4cc2dbdd914f955ebc
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-08-24 12:19:27 +0430

    fix : minor bug in generate method fixed

--- samila/genimage.py
+++ samila/genimage.py
@@ -133,16 +133,16 @@ class GenerativeImage:
                 elif self.generate_mode == GenerateMode.F2_VS_X2.value:
                     data1_ = self.function2(point[0], point[1]).real
                     data2_ = point[1]
-                elif self.generate_mode == GenerateMode.X1_VS_F1:
+                elif self.generate_mode == GenerateMode.X1_VS_F1.value:
                     data1_ = point[0]
                     data2_ = self.function1(point[0], point[1]).real
-                elif self.generate_mode == GenerateMode.X1_VS_F2:
+                elif self.generate_mode == GenerateMode.X1_VS_F2.value:
                     data1_ = point[0]
                     data2_ = self.function2(point[0], point[1]).real
-                elif self.generate_mode == GenerateMode.X2_VS_F1:
+                elif self.generate_mode == GenerateMode.X2_VS_F1.value:
                     data1_ = point[1]
                     data2_ = self.function1(point[0], point[1]).real
-                elif self.generate_mode == GenerateMode.X2_VS_F2:
+                elif self.generate_mode == GenerateMode.X2_VS_F2.value:
                     data1_ = point[1]
                     data2_ = self.function2(point[0], point[1]).real
                 self.data1.append(data1_)
