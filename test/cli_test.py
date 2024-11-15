f5b2e93d9f0aedf8837f0863c003b69f2f43afd3
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-11-15 14:04:32 +0330

    fix : cli_test.py removed

--- test/cli_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
->>> from samila.cli import *
->>> main(sys_args=["--version"])
-1.3
->>> main(sys_args=["--no-display"])
->>> main(sys_args=["--no-display", "--save-image", "test.png", "--save-config", "config.json", "--save-data", "data.json"])
->>> main(sys_args=["--no-display", "--save-image", "test.png", "--save-config", "config.json", "--save-data", "data.json", "--verbose"])
-"""
