70d717ec59a7a5b3b075a0ea7a61b211f82dc421
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-11-15 12:44:13 +0330

    fix : cli_test.py updated

--- test/cli_test.py
+++ test/cli_test.py
@@ -23,23 +23,10 @@ Samila is a generative art generator written in Python, Samila let's you
 create arts based on many thousand points. The position of every single
 point is calculated by a formula, which has random parameters.
 Because of the random numbers, every image looks different.
-
+<BLANKLINE>
 Website : https://www.samila.site
 Repo : https://github.com/sepandhaghighi/samila
 >>> main(sys_args=["--no-display"])
 >>> main(sys_args=["--no-display", "--save-image", "test.png", "--save-config", "config.json", "--save-data", "data.json"])
 >>> main(sys_args=["--no-display", "--save-image", "test.png", "--save-config", "config.json", "--save-data", "data.json", "--verbose"])
-[LOG] GenerativeImage object created.
-******************************
-[LOG] GenerativeImage generated.
-******************************
-[LOG] GenerativeImage plotted.
-******************************
-[LOG] Image saved. File address: C:\Users\Sepkjaer\AppData\Local\Programs\Python\Python38-32\test.png
-******************************
-[LOG] Data saved. File address: C:\Users\Sepkjaer\AppData\Local\Programs\Python\Python38-32\data.json
-******************************
-[LOG] Config saved. File address: C:\Users\Sepkjaer\AppData\Local\Programs\Python\Python38-32\config.json
-******************************
-
 """
