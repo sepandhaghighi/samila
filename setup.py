858ca5183ec0a8aa5816f545aeaf0ba948f56d3e
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-11-18 17:36:32 +0330

    rel : migrate to version 1.4

--- setup.py
+++ setup.py
@@ -33,14 +33,14 @@ def read_description():
 setup(
     name='samila',
     packages=['samila'],
-    version='1.3',
+    version='1.4',
     description='A Generative Art Generator',
     long_description=read_description(),
     long_description_content_type='text/markdown',
     author='Samila Development Team',
     author_email='info@samila.site',
     url='https://www.samila.site',
-    download_url='https://github.com/sepandhaghighi/samila/tarball/v1.3',
+    download_url='https://github.com/sepandhaghighi/samila/tarball/v1.4',
     keywords="generative generative-art art nft file",
     project_urls={
         'Source': 'https://github.com/sepandhaghighi/samila',
