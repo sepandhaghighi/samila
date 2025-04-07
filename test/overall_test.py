2c06ccefe6fb6fb29a08e4cac2c5ad5a1a68ced4
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-04-07 18:11:40 +0430

    fix : overall_test.py updated

--- test/overall_test.py
+++ test/overall_test.py
@@ -420,6 +420,12 @@ True
 >>> g.generate(start=-2*math.pi, step=0.1, stop=math.pi/2, mode=GenerateMode.F1F2_VS_F2)
 >>> g.generate_mode == GenerateMode.F1F2_VS_F2.value
 True
+>>> g.generate(start=-2*math.pi, step=0.1, stop=math.pi/2, mode=GenerateMode.F1_VS_F1F2)
+>>> g.generate_mode == GenerateMode.F1_VS_F1F2.value
+True
+>>> g.generate(start=-2*math.pi, step=0.1, stop=math.pi/2, mode=GenerateMode.F2_VS_F1F2)
+>>> g.generate_mode == GenerateMode.F2_VS_F1F2.value
+True
 >>> os.remove("test.png")
 >>> os.remove("test2.png")
 >>> os.remove("data.json")
