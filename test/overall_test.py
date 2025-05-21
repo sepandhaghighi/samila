bc58ff724ee7421e4eb852803850e88e62d10c42
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-05-21 14:35:26 +0330

    fix : tests updated

--- test/overall_test.py
+++ test/overall_test.py
@@ -123,37 +123,19 @@ False
 False
 >>> marker1 == marker2
 False
->>> result = g.nft_storage(api_key="")
->>> result['status']
-False
->>> result['message']
-'API Key is missing, make sure the `Authorization` header has a value in the following format `Bearer {api key}`.'
 >>> g = GenerativeImage(f1,f2)
->>> result = g.nft_storage(api_key="")
->>> result["status"]
-False
->>> result["message"]
-'No figure was found. First run `generate` and `plot` methods.'
 >>> result = g.save_image(file_adr="")
 >>> result["status"]
 False
 >>> result["message"]
 'No figure was found. First run `generate` and `plot` methods.'
 >>> g.fig = 2
->>> result = g.nft_storage(api_key="")
->>> result['status']
-False
 >>> result = g.save_image(file_adr="")
 >>> result["status"]
 False
 >>> socket.socket = guard
 >>> g.generate()
 >>> g.plot()
->>> result = g.nft_storage("")
->>> result["status"]
-False
->>> result["message"]
-'No internet connection!'
 >>> result = g.save_data(file_adr="")
 >>> result["status"]
 False
